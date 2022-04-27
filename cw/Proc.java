e517af040be54dff0226fdcee9c6889c93f98310
Ebose-star <104510377+Ebose-star@users.noreply.github.com>
2022-04-27 17:41:10 +0100

    Done

---
+++ cw/Proc.java
@@ -0,0 +1,18 @@
+package cw;
+import java.util.LinkedList;
+
+public class Proc {
+    int id;
+    LinkedList<Integer> linkedList;
+    
+    
+    public Proc(int processID){
+        this.id = id;
+    }
+    public Proc(String s){
+
+    }
+    public static void main(String[] args) {
+    }
+}
+
