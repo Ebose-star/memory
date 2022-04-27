c82e72bf3b6199c4306ca650fe10258b0b4367eb
Ebose-star <104510377+Ebose-star@users.noreply.github.com>
2022-04-27 16:46:48 +0100

    Create process.java

---
+++ cw/process.java
@@ -0,0 +1,16 @@
+package cw;
+import java.util.LinkedList;
+
+public class process{
+ private int processID;
+    private LinkedList<String> memorySegments;
+
+    public process(int processID){
+        this.processID = processID;
+    }
+    public static void main(String[] args) {
+        LinkedList<String> memorySegments = new LinkedList<String>();
+    
+    }
+}   
+
